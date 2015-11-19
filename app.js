b69b8c13060ab85c25e180be45a36aa0447460c3
AJ ONeal <awesome@coolaj86.com>
2015-11-19 06:44:34 +0000

    don't send null vals

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ exports.create = function (conf, DnsStore, app) {
   // server, store, host, port, publicDir, options
   app.use('/', function (req, res) {
     console.log('[LOG DNS API]', req.method, req.url);
-    DnsStore.Domains.find(null, { limit: 100 }).then(function (rows) {
+    DnsStore.Domains.find(null, { limit: 500 }).then(function (rows) {
+      rows.forEach(function (row) {
+        Object.keys(row).forEach(function (key) {
+          if (null === row[key] || '' === row[key]) {
+            row[key] = undefined;
+          }
+        });
+      });
       res.send(rows);
     });
   });
